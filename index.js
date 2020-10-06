b7599ccc556fc906f88aa61b67d58db2d19425f2
Manuel Toledo <mtole035@fiu.edu>
2020-10-06 01:53:28 -0400

    first deploy

--- index.js
+++ index.js
@@ -14,8 +14,4 @@ app.get('/projects/midas', async (request, response) => {
   response.send(await readFile('./src/projects/MIDAS/midas.html', 'utf8'));
 });
 
-app.get('/test', async (request, response) => {
-  response.send(await readFile('./src/loading.html', 'utf8'));
-});
-
 app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'));
