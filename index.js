7481cccb8da16d9663cc0cac110abb6a2f2cbc37
Manuel Toledo <mtole035@fiu.edu>
2020-12-07 11:49:49 -0500

    updated cards to work using boostrap grid

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ app.get('/projects/RoboticsAcademy', async (request, response) => {
   response.send(await readFile('./src/projects/RB/rb.html', 'utf8'));
 });
 
-app.get('/resume', async (request, response) => {
-  var data = readFile('./src/resume/Manuel_Toledo_Resume.pdf', 'utf8');
-  response.contentType("application/pdf");
-  response.send(data);
-  // https://stackoverflow.com/questions/31105846/how-to-send-a-pdf-file-from-node-express-app-to-the-browser/31106110
-});
+// app.get('/resume', async (request, response) => {
+//   var data = readFile('./src/resume/Manuel_Toledo_Resume.pdf', 'utf8');
+//   response.contentType("application/pdf");
+//   response.send(data);
+//   // https://stackoverflow.com/questions/31105846/how-to-send-a-pdf-file-from-node-express-app-to-the-browser/31106110
+// });
 
 app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'));
